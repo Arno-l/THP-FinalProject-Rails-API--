7189e5fa051375f6e3cd1b5bff3f4729b0440718
Non0-13 <arnaud.lucciarini@gmail.com>
2022-06-27 14:16:59 +0200

    change index product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,9 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   def index
     @products = Product.all.joins(:image_attachment)
-    render json: @products.map { |product| 
-      product.as_json().merge(
-      image_path: url_for(product.image) )}  
+    render json: @products 
   end
 
   # GET /products/1
