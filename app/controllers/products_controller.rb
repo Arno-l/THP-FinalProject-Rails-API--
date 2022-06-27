d8293bf9c13abccdd10aaec5aca051a80bf25ebf
Non0-13 <arnaud.lucciarini@gmail.com>
2022-06-27 14:20:04 +0200

    cancel changes index product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,9 @@ class ProductsController < ApplicationController
   # GET /products
   def index
     @products = Product.all.joins(:image_attachment)
-    render json: @products 
+    render json: @products.map { |product| 
+      product.as_json().merge(
+      image_path: url_for(product.image) )}  
   end
 
   # GET /products/1
