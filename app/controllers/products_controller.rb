f22d92f9d3948596b329e3cc23a8d6c4d836217e
Non0-13 <arnaud.lucciarini@gmail.com>
2022-06-27 14:27:08 +0200

    idem

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,10 +3,7 @@ class ProductsController < ApplicationController
 
   # GET /products
   def index
-    @products = Product.all.joins(:image_attachment)
-    render json: @products.map { |product| 
-      product.as_json().merge(
-      image_path: url_for(product.image) )}  
+    @products = Product.all
   end
 
   # GET /products/1
