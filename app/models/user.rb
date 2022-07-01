45e5b65e57853659c76bdb7f3f4a22f60483c201
NoahAncolie <noahancolie@gmail.com>
2022-07-01 11:15:09 +0200

    Uniqueness to Emails for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,6 @@ class User < ApplicationRecord
 	devise :database_authenticatable, :registerable,
 	:jwt_authenticatable,
 	jwt_revocation_strategy: JwtDenylist
-
-	def sendPassword
-		
-	end
+	
+	validates :email, uniqueness: true
 end
